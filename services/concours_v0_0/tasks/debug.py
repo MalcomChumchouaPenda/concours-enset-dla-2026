6af7386c063daae4bb249b63d24ce8a860c8bdab
MalcomChumchouaPenda <malcom.chumchouapenda@yahoo.fr>
2026-01-31 07:19:33 +0100

    add debug tools for incomplete inscriptions

---
+++ services/concours_v0_0/tasks/debug.py
@@ -0,0 +1,12 @@
+
+from core.auth import models as auth_mdl
+from services.concours_v0_0 import models as con_mdl
+
+
+def list_incomplete_inscriptions():
+    found = []
+    for user in auth_mdl.User.query.all():
+        query = con_mdl.InscriptionConcours.query
+        if not query.filter_by(id=user.id):
+            found.append(user)
+    return found
