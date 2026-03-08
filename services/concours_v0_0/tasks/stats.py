6ac587eabf04db9970e61f974f2832980dde817e
MalcomChumchouaPenda <malcom.chumchouapenda@yahoo.fr>
2026-03-08 16:14:45 +0100

    improve candidats list

--- services/concours_v0_0/tasks/stats.py
+++ services/concours_v0_0/tasks/stats.py
@@ -7,22 +7,52 @@ from .. import models as mdl
 
 
 def list_candidats(temp_dir):
+    Centre = mdl.CentreConcours 
+    Diplome = mdl.DiplomeConcours
+    Classe = mdl.ClasseConcours
+    Option = mdl.OptionConcours
+    Filiere = mdl.FiliereConcours
     Inscr = mdl.InscriptionConcours
-    query = Inscr.query
 
+    query = db.session.query(
+        Centre.nom.label('centre'),
+        Classe.niveau_id.label('niveau'),
+        Inscr.nom,
+        Inscr.prenom,
+        Inscr.date_naissance,
+        Inscr.lieu_naissance,
+        Inscr.sexe_id,
+        Inscr.langue_id.label('langue'),
+        Inscr.numero_dossier,
+        Diplome.nom_fr.label('diplome'),
+        Option.nom_fr.label('option'),
+        Filiere.nom_fr.label('filiere')
+    )
+
+    query = query.join(Centre)\
+                .join(Diplome)\
+                .join(Classe)\
+                .join(Option)\
+                .join(Filiere)
+    
     output_name = 'candidats_concours.csv'
     output_path = os.path.join(temp_dir, output_name)
     with open(output_path, 'w', newline='') as f:
         writer = csv.writer(f, delimiter=';')
-        writer.writerow(['numero_dossier', 
-                         'numero_paiement', 
-                         'nom_complet'])
+        writer.writerow(['centre',
+                         'niveau',
+                         'nom', 
+                         'prenom', 
+                         'date_naiss',
+                         'lieu_naiss',
+                         'genre',
+                         'langue',
+                         'numero_dossier',
+                         'option',
+                         'filiere'
+                         ])
         for row in query.all():
-            writer.writerow([
-                row.numero_dossier,
-                row.id,
-                row.nom_complet
-            ])
+            writer.writerow(row)
     return output_path
 
 
